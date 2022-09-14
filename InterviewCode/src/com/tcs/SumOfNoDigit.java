97c952a290e39be756688ad793c38bb6b1df0f15
amolmahajan <amolmahajan1998@gmail.com>
2022-09-14 14:30:17 +0530

    Code changes

---
+++ InterviewCode/src/com/tcs/SumOfNoDigit.java
@@ -0,0 +1,14 @@
+package com.tcs;
+
+public class SumOfNoDigit {
+	public static void main(String[] args) {
+		int no=123545;
+		int a=0;
+		int sum =0;
+	for (;no!=0;) {
+			a=no%10;
+			sum=sum+a;
+			no=no/10;
+			}
+		System.out.println(sum);
+		}}
